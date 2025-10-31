6a75b507596e12af3e549515a0a6bdb34e823832
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-10-30 20:34:33 -0500

    Update specs format in wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,9 +22,9 @@ export const config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: [
-        './features/**/*.feature'
-    ],
+  specs: [
+    "./features/**/*.feature", // Specs here
+  ],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
